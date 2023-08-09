4091a0f72169c2774cdad7d134f259921b2ac6a0
dev-sareno <dev.sareno@gmail.com>
2023-08-09 22:32:43 +0800

    chore: Add intial code

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/dev-sareno/lambda-simple-ses-sender
 
 go 1.20
+
+require github.com/aws/aws-sdk-go v1.44.319
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,118 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	//go get -u github.com/aws/aws-sdk-go
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
+)
+
+const (
+	// Replace sender@example.com with your "From" address.
+	// This address must be verified with Amazon SES.
+	Sender = "noreply@graphnetworks.com.au"
+
+	// Replace recipient@example.com with a "To" address. If your account
+	// is still in the sandbox, this address must be verified.
+	Recipient = "hpo.sareno@gmail.com"
+
+	// Specify a configuration set. To use a configuration
+	// set, comment the next line and line 92.
+	//ConfigurationSet = "ConfigSet"
+
+	// The subject line for the email.
+	Subject = "Form Submitted - graphnetworks.com.au"
+
+	// The HTML body for the email.
+	HtmlBody = `
+	The following details have been submitted via <a href="https://graphnetworks.com.au/">https://graphnetworks.com.au/</a>
+	<br/>
+	<br/>
+	Name: John Doe
+	<br/>
+	Company Name: ABC Company
+	<br/>
+	Company Industry: IT
+	<br/>
+	Email Address: ABC Bulding CDE
+	<br/>
+	Phone Number: 000000000000
+	<br/>
+	Message: Message body
+	`
+
+	// The character encoding for the email.
+	CharSet = "UTF-8"
+)
+
+func main() {
+	// Create a new session in the us-west-2 region.
+	// Replace us-west-2 with the AWS Region you're using for Amazon SES.
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("ap-southeast-2")},
+	)
+
+	if err != nil {
+		log.Fatalf("an error has occurred while creating seesion. %s\n", err.Error())
+	}
+
+	// Create an SES session.
+	svc := ses.New(sess)
+
+	// Assemble the email.
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(Recipient),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(HtmlBody),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(CharSet),
+				Data:    aws.String(Subject),
+			},
+		},
+		Source: aws.String(Sender),
+		// Uncomment to use a configuration set
+		//ConfigurationSetName: aws.String(ConfigurationSet),
+	}
+
+	// Attempt to send the email.
+	result, err := svc.SendEmail(input)
+
+	// Display error messages if they occur.
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ses.ErrCodeMessageRejected:
+				fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
+			case ses.ErrCodeMailFromDomainNotVerifiedException:
+				fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
+			case ses.ErrCodeConfigurationSetDoesNotExistException:
+				fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+
+		return
+	}
+
+	fmt.Println("Email Sent to address: " + Recipient)
+	fmt.Println(result)
 }
